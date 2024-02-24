c27c236c655d8e89454ed6ef415fd59eccf34cb4
Gurmeet <128692463+gurmeet00@users.noreply.github.com>
2024-02-24 19:42:13 +0530

    updation

--- src/Components/Profile.js
+++ src/Components/Profile.js
@@ -12,17 +12,18 @@ function Profile() {
    <Grid container spacing={1}>
     <Grid item xs={12} sx={{display:{xs:"block", md:"flex", lg:"block"}, justifyContent:"space-between"}}>
     <Typography variant={'h3'} sx={{fontWeight:"bold", fontSize:{xs:"40px", md:"45px"} }}>Gurmeet Singh</Typography>
-    <Typography paragraph={true} sx={{fontFamily:"Poppins", fontSize:{xs:"15px", md:"17px"}, mt:"12px", color:'#7844e9'}}> Wordpress & Frontend Developer </Typography>
+    <Typography paragraph={true} sx={{fontFamily:"Poppins", fontSize:{xs:"15px", md:"17px"}, mt:"12px", color:'#7844e9'}}> React Developer </Typography>
     </Grid>
     <Grid item xs={12} sx={{textAlign:"center", pt:"40px", borderTop:"1px solid  #909090"}}>
     <Avatar src="/profile_pic/blackandwhite1.png" alt="profile"  sx={{width:220 , height:220 , mx:'auto' , my:"30px"}}/>
-    <Typography va  riant="h5" sx={{ mt:"30px", fontSize:"30px", fontFamily:"poppins"}}>Hi, i am </Typography>
+    <Typography variant="h5" sx={{ mt:"30px", fontSize:"30px", fontFamily:"poppins"}}>Hi, i am </Typography>
+    <Typography variant="h5" className='myDesignation' sx={{ fontSize:'28px' ,color:"#7844e9" ,  fontWeight:'bold',  textShadow:'0 0 10px #7844e9' }}>React Developer </Typography>
   
-      <ul  className='secText'>
+      {/* <ul  className='secText'>
       <li><span>Wordpress Developer</span></li>
       <li><span>React Developer</span></li>
       <li><span>Freelancer</span></li>
-    </ul>
+    </ul> */}
      <Typography paragraph={true} sx={{fontFamily:"Poppins", fontSize:'15px', mt:"10  px", color:"#909090"}}>Check My Social Media Profiles</Typography>
 
 <Grid container spacing={1}>
