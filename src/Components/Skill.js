06fcb7a0e259208f773156f75054d57f7c3b6216
Gurmeet <128692463+gurmeet00@users.noreply.github.com>
2024-08-23 20:20:08 +0530

    changes

--- src/Components/Skill.js
+++ src/Components/Skill.js
@@ -1,110 +1,93 @@
-import React from 'react'
-import PersonOutlineOutlinedIcon from '@mui/icons-material/PersonOutlineOutlined';
-import Chip from '@mui/material/Chip';
-import Stack from '@mui/material/Stack';
-import { Box, Grid, Typography } from '@mui/material';
-import CollectionsOutlinedIcon from '@mui/icons-material/CollectionsOutlined';
-
+import React from "react";
+import PersonOutlineOutlinedIcon from "@mui/icons-material/PersonOutlineOutlined";
+import Chip from "@mui/material/Chip";
+import Stack from "@mui/material/Stack";
+import { Box, Grid, Typography } from "@mui/material";
+import CollectionsOutlinedIcon from "@mui/icons-material/CollectionsOutlined";
 
 function Skill() {
+  const skills = [
+    { name: "React.js", imagePath: "/profile_pic/react.png", alt: "react" },
 
+    { name: "Next.js", imagePath: "/profile_pic/nextjs.png", alt: "next" },
 
+    { name: "Javascript", imagePath: "/profile_pic/js.png", alt: "javascript" },
+    {
+      name: "Redux/Redux Tool Kit",
+      imagePath: "/profile_pic/rtk.png",
+      alt: "redux/rtk",
+    },
+    {
+      name: "React Query",
+      imagePath: "/profile_pic/react_query.png",
+      alt: "react_query",
+    },
+    { name: " Material UI", imagePath: "/profile_pic/mui.png", alt: "mui" },
+    { name: "Git", imagePath: "/profile_pic/git.png", alt: "git" },
+    {
+      name: "Wordpress",
+      imagePath: "/profile_pic/wordpress.png",
+      alt: "wordpress",
+    },
+    { name: "HTML", imagePath: "/profile_pic/html.png", alt: "html" },
+    { name: "CSS", imagePath: "/profile_pic/css_.png", alt: "css" },
+    {
+      name: "Bootstrap",
+      imagePath: "/profile_pic/bootstrap.png",
+      alt: "bootstrap",
+    },
+
+    {
+      name: "Firebase",
+      imagePath: "/profile_pic/firebase.png",
+      alt: "firbase",
+    },
+    { name: "Photoshop", imagePath: "/profile_pic/ps.png", alt: "photoshop" },
+  ];
   return (
-  <>
-    
-    <Grid item xs={12} sx={{my:5}}>
-    <Stack direction="row" spacing={1}>
-<Chip variant="outlined"
- avatar={<CollectionsOutlinedIcon style={{color:"white"}}/>} label="My Skills" sx={{color:"white"}}/>
-    </Stack>
+    <>
+      <Grid item xs={12} sx={{ my: 5 }}>
+        <Stack direction="row" spacing={1}>
+          <Chip
+            variant="outlined"
+            avatar={<CollectionsOutlinedIcon style={{ color: "white" }} />}
+            label="My Skills"
+            sx={{ color: "white" }}
+          />
+        </Stack>
 
-    <Typography sx={{fontSize:{xs:"34px", md:"60px"},fontWeight: 'regular', color:"White", mt:3, fontFamily:"poppins"}}>
-    My <span style={{color:"#7844e9"}}>  Perks</span>
-</Typography>
-<Grid container spacing={4} sx={{my:3}}>
-  <Grid item xs={6}  md={4} lg={2.5} > 
-  <Box className="logoBox">
-  <img src="/profile_pic/wordpress.png" alt="logo" width={80}/>
-{/* <Typography className="percentage"  >80%</Typography> */}
-</Box>
-    <Typography paragraph={true} className="techName" >Wordpress</Typography>
-
-  </Grid>
-
-  <Grid item xs={6}  md={4} lg={2.5}  > 
-  <Box className="logoBox">
-  <img src="/profile_pic/html.png" alt="logo" width={80}/>
-</Box>
-    <Typography paragraph={true} className="techName" >HTML</Typography>
-
-  </Grid>
-  <Grid item xs={6}  md={4} lg={2.5}  > 
-  <Box className="logoBox">
-  <img src="/profile_pic/css_.png" alt="logo" width={80}/>
-</Box>
-    <Typography paragraph={true} className="techName" >CSS</Typography>
-
-  </Grid>
-  <Grid item xs={6}  md={4} lg={2.5}  > 
-  <Box className="logoBox">
-  <img src="/profile_pic/bootstrap.png" alt="logo" width={80}/>
-</Box>
-    <Typography paragraph={true} className="techName" >Bootstrap</Typography>
-
-  </Grid>
-  <Grid item xs={6}  md={4} lg={2.5}  > 
-  <Box className="logoBox">
-  <img src="/profile_pic/js.png" alt="logo" width={80}/>
-</Box>
-    <Typography paragraph={true} className="techName" >Javascript</Typography>
-
-  </Grid>
-  <Grid item xs={6}  md={4} lg={2.5}  > 
-  <Box className="logoBox">
-  <img src="/profile_pic/react.png" alt="logo" width={80}/>
-</Box>
-    <Typography paragraph={true} className="techName" >React</Typography>
-
-  </Grid>
-  <Grid item xs={6}  md={4} lg={2.5}  > 
-  <Box className="logoBox">
-  <img src="/profile_pic/mui.png" alt="logo" width={80}/>
-</Box>
-    <Typography paragraph={true} className="techName" >Material UI</Typography>
-
-  </Grid>
-  <Grid item xs={6}  md={4} lg={2.5}  > 
-  <Box className="logoBox">
-  <img src="/profile_pic/rtk.png" alt="logo" width={80}/>
-</Box>
-    <Typography paragraph={true} className="techName" >Redux Tool Kit</Typography>
-
-  </Grid>
-  <Grid item xs={6}  md={4} lg={2.5}  > 
-  <Box className="logoBox">
-  <img src="/profile_pic/git.png" alt="logo" width={80}/>
-</Box>
-    <Typography paragraph={true} className="techName" >Git</Typography>
-
-  </Grid>
-  <Grid item xs={6}  md={4} lg={2.5}  > 
-  <Box className="logoBox">
-  <img src="/profile_pic/firebase.png" alt="logo" width={80}/>
-</Box>
-    <Typography paragraph={true} className="techName" >Firebase</Typography>
-
-  </Grid>
-  <Grid item xs={6}  md={4} lg={2.5}  > 
-  <Box className="logoBox">
-  <img src="/profile_pic/ps.png" alt="logo" width={80}/>
-</Box>
-    <Typography paragraph={true} className="techName" >Photoshop</Typography>
-
-  </Grid>
-</Grid>
-    </Grid>
-  </>
-  )
+        <Typography
+          sx={{
+            fontSize: { xs: "34px", md: "60px" },
+            fontWeight: "regular",
+            color: "White",
+            mt: 3,
+            fontFamily: "poppins",
+          }}
+        >
+          My <span style={{ color: "#7844e9" }}> Perks</span>
+        </Typography>
+        <Grid container spacing={4} sx={{ my: 3 }}>
+          {skills.map((ele, index) => {
+            return (
+              <>
+                <Grid item xs={6} md={4} lg={2.5}>
+                  <Box className="logoBox">
+                    <img src={ele.imagePath} alt={ele.alt} width={80} />
+                    {/* <Typography className="percentage"  >80%</Typography> */}
+                  </Box>
+                  <Typography paragraph={true} className="techName">
+                    {ele.name}
+                  </Typography>
+                </Grid>
+              </>
+            );
+          })}
+          <br />
+        </Grid>
+      </Grid>
+    </>
+  );
 }
 
-export default Skill
+export default Skill;
