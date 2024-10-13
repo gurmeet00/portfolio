b55c75afeff64e3ce0d3c2e57b3942e76fc26688
Gurmeet <128692463+gurmeet00@users.noreply.github.com>
2024-10-13 20:47:08 +0530

    aad infino logo

--- src/Components/Work_With.js
+++ src/Components/Work_With.js
@@ -1,28 +1,48 @@
-import React from 'react'
-import Chip from '@mui/material/Chip';
-import Stack from '@mui/material/Stack';
-import {Grid } from '@mui/material';
-import WorkOutlineOutlinedIcon from '@mui/icons-material/WorkOutlineOutlined';
+import React from "react";
+import Chip from "@mui/material/Chip";
+import Stack from "@mui/material/Stack";
+import { Grid } from "@mui/material";
+import WorkOutlineOutlinedIcon from "@mui/icons-material/WorkOutlineOutlined";
 function WorkWith() {
+  let companies = [
+    {
+      name: "Rocky_mountain_technologies_pvt_ltd",
+      img: "profile_pic/Rocky_Mountain_Logo.png",
+      companyLink: "https://rockymountaintech.co/",
+    },
+    {
+      name: "Infino",
+      img: "profile_pic/infino.png",
+      companyLink: "https://infino.co/",
+    },
+  ];
   return (
-<>
-<Grid item xs={12} sx={{my:8}}>
-    <Stack direction="row" spacing={1}>
-<Chip variant="outlined"
- avatar={<WorkOutlineOutlinedIcon style={{color:"white"}}/>} label="Work As Professional In 
-Rocky Mountain Technologies India Pvt. Ltd" sx={{color:"white"}}/>
-    </Stack>
+    <>
+      <Grid item xs={12} sx={{ my: 8 }}>
+        <Stack direction="row" spacing={1}>
+          <Chip
+            variant="outlined"
+            avatar={<WorkOutlineOutlinedIcon style={{ color: "white" }} />}
+            label="Work As Professional In 
+Rocky Mountain Technologies India Pvt. Ltd"
+            sx={{ color: "white" }}
+          />
+        </Stack>
 
-<Grid item xs={12} sx={{my:5}}>
-<a href='https://rockymountaintech.co/' target='_blank'>
-    <img src="profile_pic/rockymountain_logo.png" alt='rockymountain' width={140}/>
-   </a>
-</Grid>
-
-    </Grid>
-
-</>
-  )
+        <Grid container spacing={5} sx={{ my: "10px", alignItems: "center" }}>
+          {companies.map((item) => (
+            <>
+              <Grid item xs={6} sm={4} md={3}>
+                <a href={item.companyLink} target="_blank">
+                  <img src={item.img} alt={item.name} width="60%" />
+                </a>
+              </Grid>
+            </>
+          ))}
+        </Grid>
+      </Grid>
+    </>
+  );
 }
 
-export default WorkWith
+export default WorkWith;
